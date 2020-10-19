6d8bac1b2bc537bf5b18762fc52be1cd13225122
NoahCardoza <noahcardoza@gmail.com>
2020-10-19 00:31:40 -0700

    added inline /pin and support 📌 as a prefix

--- app/bot.py
+++ app/bot.py
@@ -4,13 +4,28 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix='/')
+
+reaction_emoji = '📌'
+
+prefix_map = {
+    reaction_emoji: ''
+}
 
 
-def main():
-    load_dotenv()
-    sys.stdout.flush()
-    bot.run(os.getenv("TOKEN"))
+def select_command_prefix(_bot, message):
+    return prefix_map.get(message.clean_content[0], '/')
+
+
+bot = commands.Bot(command_prefix=select_command_prefix,
+                   description=f"You can now react to messages with {reaction_emoji} to pin a message!")
+
+
+@bot.event
+async def on_ready():
+    print("Ready!")
+    activity = discord.Activity(name="/help pin to get started!",
+                                type=discord.ActivityType.watching)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
 
 
 @bot.event
@@ -18,8 +33,16 @@ async def on_raw_reaction_add(payload):
     channel = await bot.fetch_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
 
-    if not message.pinned and payload.emoji.name == '📌':
-        await message.pin()
+    if payload.emoji.name == reaction_emoji:
+        if not message.pinned:
+            await message.pin()
+        else:
+            pin_reaction = {
+                r.emoji: r for r in message.reactions
+            }.get(reaction_emoji)
+
+            if pin_reaction and pin_reaction.me:
+                await message.remove_reaction(reaction_emoji, bot.user)
 
 
 @bot.event
@@ -27,44 +50,43 @@ async def on_raw_reaction_remove(payload):
     channel = await bot.fetch_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
 
-    if payload.emoji.name == '📌' and message.pinned:
+    if payload.emoji.name == reaction_emoji and message.pinned:
         await message.unpin()
+        await message.clear_reaction(reaction_emoji)
+        await channel.send("Unpinned!")
 
 
-@bot.event
-async def on_ready():
-    print("Ready!")
-    activity = discord.Activity(name="/pin {url} to pin a message!",
-                                type=discord.ActivityType.watching)
-    await bot.change_presence(status=discord.Status.online, activity=activity)
-
-
-@bot.command(description="Pin a message. Argument: URL of message to be pinned")
+@bot.command(description="Pin a message. Argument: URL of the  message to be pinned or message itself", aliases=[reaction_emoji])
 async def pin(ctx):
-    message = await get_message(ctx)
     try:
+        message = await get_message_from_url(ctx)
+        if not message:
+            message = ctx.message
+
         if message.pinned == True:
             await ctx.send("Message is already pinned")
         else:
-            await message.pin()
+            # this will trigger on_raw_reaction_add to actually pin the message
+            await message.add_reaction(reaction_emoji)
     except Exception:
         await ctx.send("Something went wrong. Make sure your URL is correct and valid.")
 
 
 @bot.command(description="Unpin a message. Argument: URL of message to be unpinned")
 async def unpin(ctx):
-    message = await get_message(ctx)
+    message = await get_message_from_url(ctx)
+
     try:
         if message.pinned == False:
             await ctx.send("Message is not pinned")
         else:
-            await message.unpin()
-            await ctx.send("Unpinned!")
-    except:
+            # this will trigger on_raw_reaction_remove to actually unpin the message
+            await message.remove_reaction(reaction_emoji, bot.user)
+    except Exception:
         await ctx.send("Something went wrong. Make sure your URL is correct and valid.")
 
 
-async def get_message(ctx):
+async def get_message_from_url(ctx):
     message_url = ctx.message.clean_content.split(' ')[1]
 
     # Check if url is from Discord
@@ -79,6 +101,13 @@ async def get_message(ctx):
             return None
     else:
         return None
+
+
+def main():
+    load_dotenv()
+    sys.stdout.flush()
+    bot.run(os.getenv("TOKEN"))
+
 
 if __name__ == '__main__':
     main()
